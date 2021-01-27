2a16283783230760f8948b0a3e8d6d9cf8186f19
Emmanuel Tychon (etychon) <etychon@cisco.com>
2021-01-27 13:59:29 +0100

    initial commit

---
+++ api-test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+
+import requests, json
+import constants
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# constants
+debug = False
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+task = {"username":constants.RAINIER_USERNAME,"password":constants.RAINIER_PASSWORD}
+
+### REQUESTING TOKEN FOR AUTHENTICATION 
+
+print("Authenticating...")
+resp = requests.post(constants.RAINIER_BASEURL+'/iam/auth/token', 
+	json=task, 
+	verify=False)
+if resp.status_code != 200:
+    # This means something went wrong.
+	if debug: print('**ERROR** ' , resp.status_code, ' ', resp.reason)
+	exit(1)
+
+print('Request returned ' , resp.status_code, ' ', resp.reason)
+resp_json = resp.json()
+if debug: print(resp_json)
+    
+access_token = resp.json()['access_token']
+
+### GETTING ALL TENANTS AND ROLES FOR THIS USER
+
+print("Requesting tenants list and roles...")
+headers = {
+	"Content-Type" : "application/json",
+	"Authorization" : "Bearer " + access_token, 
+	"x-access-token" : access_token}
+
+resp = requests.get(constants.RAINIER_BASEURL+'/iam/users/me',
+	headers=headers,
+	verify=False)
+
+if resp.status_code != 200:
+    # This means something went wrong.
+	print('**ERROR** ' , resp.status_code, ' ', resp.reason)
+	exit(1)
+
+if debug: print('Request returned ' , resp.status_code, ' ', resp.reason)
+	
+if debug: print(resp.json())
+
+for z in resp.json()['roles']:
+	print(z['tenant_name'], ' ', z['tenant_id'], ' ', z['role_name'])
+
+### GETTING ALL DEVICES FOR A SPECIFIC TENANT
+
+print("Requesting device list for tenant ID %s..." % constants.RAINIER_TENANTID)
+headers = {
+	"Content-Type" : "application/json",
+	"Authorization" : "Bearer " + access_token, 
+	"x-access-token" : access_token,
+	"x-tenant-id": constants.RAINIER_TENANTID}
+	
+resp = requests.get(constants.RAINIER_BASEURL+'/resource/rest/api/v1/devices',
+	headers=headers,
+	verify=False)
+if resp.status_code != 200:
+    # This means something went wrong.
+	print('**ERROR** ' , resp.status_code, ' ', resp.reason)
+	exit(1)
+
+if debug: print('Request returned ' , resp.status_code, ' ', resp.reason)
+	
+if debug: print(resp.json())
+
+for z in resp.json()['results']:
+	print(z['name'],' ',z['status'])
