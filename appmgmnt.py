1f5c7b61e0525702c786b86aa310ef897e924fac
etychon <etychon@gmail.com>
2022-02-02 21:17:02 +0100

    add initial CLI parser

--- appmgmnt.py
+++ appmgmnt.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 
+import argparse
 import requests, json
 import constants
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+parser = argparse.ArgumentParser(prog='PROG', description='Process IoT OD gateways with application management.')
+parser.add_argument('--refresh', '-r', choices=['error', 'null', 'all'], help='refresh application management on gateways with errors, null or all')
+parser.add_argument('--verbose', '-v', help='verbose output', action='store_true')
+args = parser.parse_args()
+
 # constants
-debug = True 
+debug = args.verbose
 use_API_Key = False
 
+if debug: print("[INFO] verbose output is enabled")
+
+# don't warn if HTTPS connections are not valid
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def refresh_iox_device(devId):
-	print("Refreshing " + devId)
+
+	if debug: print("Refreshing device "+devId)
 
 	headers = {
         	"Content-Type" : "application/json",
@@ -20,7 +30,6 @@ def refresh_iox_device(devId):
         	"X-Tenant-ID": constants.RAINIER_TENANTID}
 
 	myobj = '{"actionType": "RefreshInventory"}'
-	print(myobj)
 
 	resp = requests.post(constants.RAINIER_BASEURL+'/appmgmt/devices/'+devId+'/action',
         	headers=headers,verify=False, data = myobj)
@@ -33,7 +42,7 @@ def refresh_iox_device(devId):
         # This means something went wrong.
         	if debug: print('**ERROR** ' , resp.status_code, ' ', resp.reason)
 	else:
-		print('Request returned ' , resp.status_code, ' ', resp.reason)
+		if debug: print('Request returned ' , resp.status_code, ' ', resp.reason)
 
 if (not use_API_Key):
 
@@ -52,7 +61,7 @@ if (not use_API_Key):
 
 		print('Request returned ' , resp.status_code, ' ', resp.reason)
 		resp_json = resp.json()
-		#if debug: print(resp_json)
+		if debug: print(resp_json)
     
 		access_token = resp.json()['access_token']
 
@@ -109,18 +118,22 @@ while True:
     		exit(1)
 
 	else:
-		#if debug: print(resp.json())
+		if debug: print(json.dumps(resp.json(), indent=2))
 		output.extend(resp.json()['data'])
 		print(".", end='')
-		#if debug: print('Request returned ' , resp.status_code, ' ', resp.reason)
+		if debug: print('Request returned ' , resp.status_code, ' ', resp.reason)
 		if 'next' in resp.json():
 			next = resp.json()['next'].split('?',1)[1]
 		else:
 			break
 
+# Initialize lists of devices (strings)
 devices_ok = ""
 devices_unreach = ""
 devices_with_errors = ""
+
+if args.refresh == "all" or args.refresh == "null" or args.refresh == "error": 
+	print("[INFO] Refreshing devices: ")
 
 for z in output:
 	if z['status'] == "DISCOVERED":
@@ -130,15 +143,22 @@ for z in output:
 	    	if not zz['name'].startswith("Group=") and not zz['name'].startswith("Type="):
 	    		apps.append(zz['name'])
 	    devices_ok = devices_ok + str("{:<20} | {:<15} | {:<15}".format(z['userProvidedSerialNumber'], z['status'], str(apps)))+'\n'
+	    if args.refresh == "all" : 
+	    	print(".", end='')
+	    	refresh_iox_device(z['deviceId']) 
 	    continue
 	if (('errorMessage' in z) and (z['errorMessage'] == "Device unreachable : null")):
 	    devices_unreach = devices_unreach + str("{:<20} | {:<15} | {:<15}".format(z['userProvidedSerialNumber'], z['status'], z['errorMessage'])) + '\n'
+	    if args.refresh == "all" or args.refresh == "null": 
+	        print(".", end='')                                                                                                                                                   
+	        refresh_iox_device(z['deviceId'])
 	    continue
     
 	devices_with_errors = devices_with_errors + str("{:<20} | {:<15} | {:<15}".format(z['userProvidedSerialNumber'], z['status'], z['errorMessage'])) + '\n'
 
-	# Let's refresh devices with errors
-	refresh_iox_device(z['deviceId'])
+	if args.refresh == "all" or args.refresh == "error":
+		print(".", end='')
+		refresh_iox_device(z['deviceId'])
 
 print("")
 print("-[ OK ]------------------------------")
@@ -149,3 +169,4 @@ print(devices_unreach)
    	
 print("-[ ERRORS ]--------------------------")
 print(devices_with_errors)
+
