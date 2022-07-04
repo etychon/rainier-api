d8a0b90ab450d51680b29d18125ae9ba93320ec3
Emmanuel Tychon (etychon) <etychon@cisco.com>
2022-07-04 12:05:26 +0200

    add search functions and caching to avoid unneded API calls

--- rainier-library-test.py
+++ rainier-library-test.py
@@ -4,12 +4,17 @@ import rainierlib.rainierapi
 import constants
 import argparse
 
+
 def is_not_blank(s):
     return bool(s and not s.isspace())
 
-parser = argparse.ArgumentParser(prog='PROG', description='Process IoT OD gateways with Edge Device Manager API')
-parser.add_argument('--verbose', '-v', help='verbose output', action='store_true')
-parser.add_argument('--tenant', '-t', help='tenant nickname as defined in constants.py')
+
+parser = argparse.ArgumentParser(
+    prog='PROG', description='Process IoT OD gateways with Edge Device Manager API')
+parser.add_argument('--verbose', '-v',
+                    help='verbose output', action='store_true')
+parser.add_argument(
+    '--tenant', '-t', help='tenant nickname as defined in constants.py')
 args = parser.parse_args()
 
 # Read tenant nick name from CLI args if provided
@@ -19,7 +24,8 @@ if args.tenant:
 # Load current tenant credentials from constants (see README.md file for how to do this)
 constants.load_tenant_creds(constants.tenant_name)
 
-print("Using tenant = {}, url = {}".format(constants.tenant_name, constants.RAINIER_BASEURL))
+print("Using tenant = {}, url = {}".format(
+    constants.tenant_name, constants.RAINIER_BASEURL))
 
 # Instanciate the rainierlib class
 rl = rainierlib.rainierapi.rainierlib()
@@ -27,8 +33,19 @@ rl = rainierlib.rainierapi.rainierlib()
 # Load all parameters for cluster, authentication, username, keys, etc... for this tenant
 rl.loadTParameters(constants)
 
+#print("Getting list #1")
+## Get all devices in this tenant
+#print(rl.getAllDevices())
+
+#print("Getting list #2")
 # Get all devices in this tenant
-print(rl.getAllDevices())
+#print("Getting all devices in this organisation...")
+#print(rl.getAllDevices())
+
+print("Getting all details of this gateway's SN...")
+print(rl.getDeviceDetails(sn="FCW2424P02X"))
+
+print("Getting all details of this gateway's name...")
+print(rl.getDeviceDetails(device_name="egon-ir1101-1"))
 
 print("Done.")
-
